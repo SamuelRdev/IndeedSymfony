386ad290eafbb6a1a5ff63a4d3ef1f0372c60ab6
Samuel R <the.sam.r@hotmail.fr>
2021-07-04 17:35:23 +0200

    Fixtures update

--- src/DataFixtures/ContractFixtures.php
+++ src/DataFixtures/ContractFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+
+use App\Entity\Contract;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ContractFixtures extends Fixture
 {
+
+    const CONTRACTS = ["CDI", "CDD", "FREE"];
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        foreach(self::CONTRACTS as $key => $value) {
+            
+            $slug = strtolower(trim($value));
+
+            $contract = new Contract();
+            $contract->setName($value);
+            $contract->setSlug($slug);
+            
+            
+
+            $this->addReference("contract_$key", $contract);
+            $manager->persist($contract);
+        }
+
 
         $manager->flush();
     }
--- src/DataFixtures/ContractTypeFixtures.php
+++ src/DataFixtures/ContractTypeFixtures.php
@@ -2,15 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ContractType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ContractTypeFixtures extends Fixture
 {
+    const CONTRACT_TYPES = ["Temps plein", "Temps partiel"];
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        foreach(self::CONTRACT_TYPES as $key => $value) {
+            
+            $slug = strtolower(trim($value));
+
+            $contract_type = new ContractType();
+            $contract_type->setName($value);
+            $contract_type->setSlug($slug);
+            
+            
+
+            $this->addReference("contract_type_$key", $contract_type);
+            $manager->persist($contract_type);
+        }
+
 
         $manager->flush();
     }
