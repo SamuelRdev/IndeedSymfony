d6515e530e4894e2783842339e956b732c82e513
Samuel R <the.sam.r@hotmail.fr>
2021-07-05 12:01:01 +0200

    maj

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Offer;
+use App\Repository\OfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/', name: 'app')]
-    public function index(): Response
+    #[Route('/', name: 'offers.list')]
+    public function index(OfferRepository $repo): Response
     {
+        $offer_list = $repo->findAll();
+
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            "offer_list" => $offer_list
+        ]);
+    }
+
+    #[Route('/offers/{id}', name: 'offers.show')]
+    public function show(OfferRepository $repo, Request $request, Offer $offer)
+    {
+        return $this->render('app/offer.html.twig', [
+            "offer" => $offer
         ]);
     }
 }
